1f72c44132e8b2dc017f15dca6d16ed8760f32e8
Devanshu Shah <dastaryt720@gmail.com>
2024-09-16 15:07:36 -0400

    Updated settings.py

--- patientrecords/settings.py
+++ patientrecords/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3$25li09o^w-o5!^^c7shil5h4#1+88=uo$i8fa(s0fw$2v6c4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -161,5 +161,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dashytisbest@gmail.com'
-EMAIL_HOST_PASSWORD = 'peuehggqasvdbwmr'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
